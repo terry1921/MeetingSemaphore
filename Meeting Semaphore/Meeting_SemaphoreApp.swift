97a84008d91187d020cb4ff852ff15bc15e02d8e
Enrique De Jesús Espinoza Salas <terryrockstar22@gmail.com>
2023-06-13 11:15:10 -0600

    Initial Commit

---
+++ Meeting Semaphore/Meeting_SemaphoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meeting_SemaphoreApp.swift
+//  Meeting Semaphore
+//
+//  Created by Enrique Espinoza on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Meeting_SemaphoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
