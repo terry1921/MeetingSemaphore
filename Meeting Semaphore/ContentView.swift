f8f37c0d9db7625aa0bff1e852ba8c73e528765c
Enrique De Jesús Espinoza Salas <terryrockstar22@gmail.com>
2023-06-13 14:09:24 -0600

    feat: Add semaphore view

--- Meeting Semaphore/ContentView.swift
+++ Meeting Semaphore/ContentView.swift
@@ -8,12 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var didTap:Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Meeting Semaphore")
+                .font(.largeTitle)
+            Spacer()
+            Image(systemName: "circle.dashed.inset.filled")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(didTap ? Color.red : Color.green)
+                .frame(width: 300.0, height: 300.0, alignment: .center)
+                .onTapGesture {
+                    self.didTap = !self.didTap
+                }
+            Spacer()
         }
         .padding()
     }
